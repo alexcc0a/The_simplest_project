7123378ec83d60119cbae56f035d9f9b23b810bb
Alexander Nesterov <alex18nest@icloud.com>
2024-09-08 16:09:31 -0300

    Initial Commit

---
+++ The_simplest_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  The_simplest_project
+//
+//  Created by Александр Нестеров on 8/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
